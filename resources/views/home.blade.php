02f61590fb90de4f903e69da5c5e98656f6f0d0a
SzenteP <22157560+szentep@users.noreply.github.com>
2021-09-12 23:41:42 +0200

    Homepage card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -149,9 +149,23 @@ Hali!!
         </div>
         <div class="card-action">
           <a href="http://example.com">Let's go!</a>
+<div class="row">
+    <div class="col s12 m7">
+      <div class="card">
+        <div class="card-image">
+          <img src="hamlet.jpg">
+          <span class="card-title">Choose wisely</span>
+        </div>
+        <div class="card-content">
+          <p>"To be, or not to be, that is the question."</p>
+        </div>
+        <div class="card-action">
+          <a href="https://youtu.be/dQw4w9WgXcQ" target="blank">Tubi <i class="small material-icons">sentiment_very_satisfied</i></a>
+          <a href="https://suicidepreventionlifeline.org/" target="blank">Nottubi <i class="small material-icons">sentiment_very_dissatisfied</i></a>
         </div>
       </div>
     </div>
   </div>
+
 
 @endsection
