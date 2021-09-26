2fb2af94c451873b6face64e9799d85b3b6772a2
Barbara <szabo.barbara.noemi@gmail.com>
2021-09-26 21:47:26 +0200

    added names to the cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
     <div class="col s2">
       <div class="card blue-grey darken-1">
         <div class="card-content white-text">
-          <span class="card-title">Card Title</span>
+          <span class="card-title">Hello George!</span>
           <p>I am a card. My name is George.</p>
         </div>
       </div>
@@ -17,7 +17,7 @@
     <div class="col s8">
       <div class="card blue-grey darken-1">
         <div class="card-content white-text">
-          <span class="card-title">Card Title</span>
+          <span class="card-title">Hello Felician!</span>
           <p>I am a card too. I need attention and more place. My name is Felician.
             And I have to write here some more stuff. 
           </p>
@@ -28,27 +28,11 @@
     <div class="col s2">
       <div class="card blue-grey darken-1">
         <div class="card-content white-text">
-          <span class="card-title">Card Title</span>
+          <span class="card-title">Hello Anonymous!</span>
           <p>I'm the last card. I don't have a name.</p>
         </div>
       </div>
     </div>
 </div>
-
-<div class="divider"></div>
-  <div class="section">
-    <h5>Section 1</h5>
-    <p>Stuff</p>
-  </div>
-  <div class="divider"></div>
-  <div class="section">
-    <h5>Section 2</h5>
-    <p>Stuff</p>
-  </div>
-  <div class="divider"></div>
-  <div class="section">
-    <h5>Section 3</h5>
-    <p>Stuff</p>
-  </div>
 
 @endsection
